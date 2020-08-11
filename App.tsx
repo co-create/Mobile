a8b0699f176207d2f121a4f101f5c3f3b9506872
Benjamin Kim <benkim96@gmail.com>
2020-08-11 14:21:20 -0700

    mvp checkpoint, vertical alignment and text input

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ export default class App extends React.Component {
 		distance: 0,
 		mode: 'in',
 		maxDistance: 400,
+		distanceInput: 400,
 	}
 
 	constructor(props) {
@@ -47,53 +48,76 @@ export default class App extends React.Component {
 		}
 	}
 
-	computeDistance():string {
+	computeDistance():number {
 		if (this.state.mode == "in") {
-			return (this.state.distance).toString();
+			return (this.state.distance);
 		} else {
-			return (this.state.distance * 2.54).toString();
+			return (this.state.distance * 2.54);
 		}
 	}
 
-	onSubmitEditing() {
+	computeColor():string {
+		let maxDistance: number = this.state.maxDistance;
+		let distance: number = this.state.distance;
+
+		if (distance <= 0.05 * maxDistance) {
+			return "red";
+		} else if (distance <= 0.2 * maxDistance) {
+			return "yellow";
+		} else {
+			return "green";
+		}
+	}
+
+	onChangeText(text: string) {
 		this.setState({
-			maxDistance: parseInt(this.state.maxDistance)
+			distanceInput: parseInt(text)
 		});
 	}
 
 	render() {
 		return (
 		<View style={styles.container}>
-			<View style={styles.column}>
-				<View style={styles.graph}>
-					{this.state.isLoading ? <ActivityIndicator/> : (
-					<View style={{
-						backgroundColor: "blue",
-						height: 400 * this.state.distance / this.state.maxDistance,
-					}}/>
-					)}
-				</View>
-				<Text style={{textAlign:"center"}}>{this.computeDistance()} {this.state.mode}</Text>
+			<View style={styles.graph}>
+				{this.state.isLoading ? <ActivityIndicator/> : (
+				<View style={{
+					backgroundColor: this.computeColor(),
+					height: 400 * this.computeDistance() / this.state.maxDistance,
+				}}/>
+				)}
 			</View>
-			<View style={styles.column}>
-				<View style={styles.button_bar}>
-					<TouchableOpacity
-						onPress={() => this.toggleMeasurement("cm")}
-						style={styles.button}
-					>
-						<Text>IN</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						onPress={() => this.toggleMeasurement("in") }
-						style={styles.button}
-					>
-						<Text>CM</Text>
-					</TouchableOpacity>
-				</View>
-				<TextInput
-					onSubmitEditing={() => this.onSubmitEditing()}
-					value={this.state.maxDistance.toString()}
+			<Text style={{textAlign:"center"}}>{this.computeDistance()} {this.state.mode}</Text>
+			<Text style={{textAlign:"center"}}>{Math.round(this.computeDistance() / this.state.maxDistance * 100)} %</Text>
+			<View style={{height:60}}/>
+			<Text style={{fontSize: 18}}>Measurement System</Text>
+			<View style={styles.button_bar}>
+				<TouchableOpacity
+					onPress={() => this.toggleMeasurement("cm")}
+					style={styles.button}
+				>
+					<Text style={{color: "white"}}>IN</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => this.toggleMeasurement("in") }
+					style={styles.button}
+				>
+					<Text style={{color: "white"}}>CM</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={{height:20}}/>
+			<Text style={{fontSize: 18}}>Max Height</Text>
+			<View style = {styles.button_bar}>
+				<TextInput style = {styles.input}
+					onChangeText = {(text) => this.onChangeText(text)}
+					placeholder = {this.state.distanceInput.toString()}
 				/>
+				<TouchableOpacity
+					style = {styles.button}
+					onPress = {
+						() => this.setState({maxDistance: this.state.distanceInput})
+					}>
+					<Text style = {{color:"white"}}> Save </Text>
+				</TouchableOpacity>
 			</View>
 		</View>
 		);
@@ -102,17 +126,11 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
 	container: {
-		flexDirection: 'row',
-	},
-	column: {
-		flex: 1,
 		flexDirection: 'column',
-		backgroundColor: 'white',
-		borderColor: "black",
-		borderWidth: 2,
-		alignItems: 'center',
-		padding: 50,
-		margin: 10,
+		alignItems: "center",
+		justifyContent: "center",
+		height: "100%",
+		width: "100%",
 	},
 	graph: {
 		backgroundColor: "white",
@@ -120,17 +138,26 @@ const styles = StyleSheet.create({
 		borderWidth: 2,
 		height: 400,
 		width: '60%',
-		margin: 50,
 		padding: 10,
+		marginTop: 20,
+		marginBottom: 20,
 		justifyContent: 'flex-end',
 	},
 	button_bar: {
 		flexDirection: 'row',
+		alignItems: "center",
 	},
 	button: {
 		alignItems: "center",
-		backgroundColor: "#DDDDDD",
+		backgroundColor: "black",
 		padding: 10,
 		margin: 10,
+		height: 40,
+	},
+	input: {
+		height: 40,
+		width: 70,
+		borderColor: 'black',
+		borderWidth: 1
 	},
 });
